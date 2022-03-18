bf038c5fd288ea42fa09f0bbe2d5d8d1739f8bfb
League Student <league@iMac-12.attlocal.net>
2022-03-17 18:31:10 -0700

    initial

---
+++ src/test/java/org/jointheleague/api/NBAStats/NbaStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.NBAStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
